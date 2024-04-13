576fa7af46896c3adb5e6aef9d956366ac8cb939
nobixz <166090657+nobixz@users.noreply.github.com>
2024-04-14 00:57:18 +0530

    Update resume.py

--- DAXXMUSIC/plugins/admins/resume.py
+++ DAXXMUSIC/plugins/admins/resume.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.utils.database import is_music_playing, music_on
-from DAXXMUSIC.utils.decorators import AdminRightsCheck
-from DAXXMUSIC.utils.inline import close_markup
+from NOBITAMUSIC import app
+from NOBITAMUSIC.core.call import NOBITA
+from NOBITAMUSIC.utils.database import is_music_playing, music_on
+from NOBITAMUSIC.utils.decorators import AdminRightsCheck
+from NOBITAMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await DAXX.resume_stream(chat_id)
+    await NOBITA.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
