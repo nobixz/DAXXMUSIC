af07cc71db03e8aa4b4bc877f14206f7dd4b74f7
nobixz <166090657+nobixz@users.noreply.github.com>
2024-04-14 01:09:21 +0530

    Update stop.py

--- DAXXMUSIC/plugins/admins/stop.py
+++ DAXXMUSIC/plugins/admins/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.utils.database import set_loop
-from DAXXMUSIC.utils.decorators import AdminRightsCheck
-from DAXXMUSIC.utils.inline import close_markup
+from NOBITAMUSIC import app
+from NOBITAMUSIC.core.call import NOBITA
+from NOBITAMUSIC.utils.database import set_loop
+from NOBITAMUSIC.utils.decorators import AdminRightsCheck
+from NOBITAMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -16,7 +16,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await DAXX.stop_stream(chat_id)
+    await NOBITA.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
