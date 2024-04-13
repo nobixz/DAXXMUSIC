8be7c6a4141788ff0694bf18f1a266d70ae8fdfc
nobixz <166090657+nobixz@users.noreply.github.com>
2024-04-14 01:17:30 +0530

    Update unmute.py

--- DAXXMUSIC/plugins/admins/unmute.py
+++ DAXXMUSIC/plugins/admins/unmute.py
@@ -1,7 +1,7 @@
-from DAXXMUSIC import app
+from NOBITAMUSIC import app
 from pyrogram import filters,enums
 from pyrogram.types import ChatPermissions 
-from DAXXMUSIC.utils.daxx_ban import admin_filter
+from NOBITAMUSIC.utils.daxx_ban import admin_filter
 
 @app.on_message(filters.command("unmuteall") & admin_filter)
 async def unmute_all(_,msg):
