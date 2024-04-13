213334303b6b556e3345bb16028db3b48ff5af0b
nobixz <166090657+nobixz@users.noreply.github.com>
2024-04-13 23:17:58 +0530

    Update call.py

--- DAXXMUSIC/core/call.py
+++ DAXXMUSIC/core/call.py
@@ -35,7 +35,7 @@ from NOBITAMUSIC.utils.exceptions import AssistantErr
 from NOBITAMUSIC.utils.formatters import check_duration, seconds_to_min, speed_converter
 from NOBITAMUSIC.utils.inline.play import stream_markup
 from NOBITAMUSIC.utils.stream.autoclear import auto_clean
-from DAXXMUSIC.utils.thumbnails import get_thumb
+from NOBITAMUSIC.utils.thumbnails import get_thumb
 from strings import get_string
 
 autoend = {}
