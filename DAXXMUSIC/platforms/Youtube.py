7058afd05e1d12ac514eb4b0f326393d61ca01f7
nobixz <166090657+nobixz@users.noreply.github.com>
2024-04-13 23:50:38 +0530

    Update Youtube.py

--- DAXXMUSIC/platforms/Youtube.py
+++ DAXXMUSIC/platforms/Youtube.py
@@ -10,8 +10,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from DAXXMUSIC.utils.database import is_on_off
-from DAXXMUSIC.utils.formatters import time_to_seconds
+from NOBITAMUSIC.utils.database import is_on_off
+from NOBITAMUSIC.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
