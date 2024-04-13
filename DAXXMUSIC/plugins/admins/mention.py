3780dd44dbe3117124a7446653df15c776b84253
nobixz <166090657+nobixz@users.noreply.github.com>
2024-04-14 00:52:58 +0530

    Update mention.py

--- DAXXMUSIC/plugins/admins/mention.py
+++ DAXXMUSIC/plugins/admins/mention.py
@@ -1,8 +1,8 @@
 import asyncio
 from pyrogram.enums import ChatType, ChatMemberStatus
-from DAXXMUSIC import app
+from NOBITAMUSIC import app
 from pyrogram import filters
-from DAXXMUSIC.utils.daxx_ban import admin_filter
+from NOBITAMUSIC.utils.daxx_ban import admin_filter
 
 
 
