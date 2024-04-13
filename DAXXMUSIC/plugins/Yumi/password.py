eefc619e774eaac1b71228903f27ffb3a9cb7c36
nobixz <166090657+nobixz@users.noreply.github.com>
2024-04-14 00:16:02 +0530

    Update password.py

--- DAXXMUSIC/plugins/Yumi/password.py
+++ DAXXMUSIC/plugins/Yumi/password.py
@@ -1,6 +1,6 @@
 import random, os
 from pyrogram import Client, filters, enums 
-from DAXXMUSIC import app
+from NOBITAMUSIC import app
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
