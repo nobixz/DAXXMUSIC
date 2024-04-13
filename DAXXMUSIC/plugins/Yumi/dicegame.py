ec3165959014cbcd9533c50829d273ae18015d55
nobixz <166090657+nobixz@users.noreply.github.com>
2024-04-13 23:54:33 +0530

    Update dicegame.py

--- DAXXMUSIC/plugins/Yumi/dicegame.py
+++ DAXXMUSIC/plugins/Yumi/dicegame.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, enums, filters
 #from config import *
 import asyncio
-from DAXXMUSIC import app as app
+from NOBITAMUSIC import app as app
 
 from pyrogram.handlers import MessageHandler
 
