e2de99c2ff37acf06939f17040b44a4778477d3b
nobixz <166090657+nobixz@users.noreply.github.com>
2024-04-13 23:29:34 +0530

    Update notesdb.py

--- DAXXMUSIC/mongo/notesdb.py
+++ DAXXMUSIC/mongo/notesdb.py
@@ -1,6 +1,6 @@
-from DAXXMUSIC.utils.mongo import db
+from NOBITAMUSIC.utils.mongo import db
 
-#from DAXXMUSIC.mongo import *# back...............
+#from NOBITAMUSIC.mongo import *# back...............
 
 notes = db.notes["notes"]
 
