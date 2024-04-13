701eafb1bfe390ae895d8d02885e3ad7886538bf
nobixz <166090657+nobixz@users.noreply.github.com>
2024-04-14 01:11:39 +0530

    Update unbanall.py

--- DAXXMUSIC/plugins/admins/unbanall.py
+++ DAXXMUSIC/plugins/admins/unbanall.py
@@ -1,8 +1,8 @@
-from DAXXMUSIC import app
+from NOBITAMUSIC import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from DAXXMUSIC.utils.daxx_ban import admin_filter
+from NOBITAMUSIC.utils.daxx_ban import admin_filter
 
 BOT_ID = "6974730627"
 
