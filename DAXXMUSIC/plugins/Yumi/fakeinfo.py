54d8d8d799a8ae008059a9d54b3618b6e349d95b
nobixz <166090657+nobixz@users.noreply.github.com>
2024-04-13 23:56:30 +0530

    Update fakeinfo.py

--- DAXXMUSIC/plugins/Yumi/fakeinfo.py
+++ DAXXMUSIC/plugins/Yumi/fakeinfo.py
@@ -1,7 +1,7 @@
 """***
 MIT License
 
-Copyright (c) [2023] [DAXX TEAM]
+Copyright (c) [2023] [Nobita TEAM]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +23,7 @@ SOFTWARE.***
 """
 
 import requests
-from DAXXMUSIC import app
+from NOBITAMUSIC import app
 from pyrogram import filters
 
 
