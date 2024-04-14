d3525601c6fc1781e01a62dbfa7b8defb6f59176
nobixz <166090657+nobixz@users.noreply.github.com>
2024-04-14 07:42:22 +0530

    Update language.py

--- DAXXMUSIC/utils/decorators/language.py
+++ DAXXMUSIC/utils/decorators/language.py
@@ -1,5 +1,5 @@
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import get_lang, is_maintenance
+from NOBITAMUSIC.misc import SUDOERS
+from NOBITAMUSIC.utils.database import get_lang, is_maintenance
 from strings import get_string
 
 
