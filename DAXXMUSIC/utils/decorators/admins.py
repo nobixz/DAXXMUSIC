7c4604c68013d8a6697d6ecd9c67c4b3007d527c
nobixz <166090657+nobixz@users.noreply.github.com>
2024-04-14 07:44:27 +0530

    Update admins.py

--- DAXXMUSIC/utils/decorators/admins.py
+++ DAXXMUSIC/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS, db
-from DAXXMUSIC.utils.database import (
+from NOBITAMUSIC import app
+from NOBITAMUSIC.misc import SUDOERS, db
+from NOBITAMUSIC.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
