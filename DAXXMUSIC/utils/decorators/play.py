67880d26a3366c22a320f9ec0869af2a439d5b9a
nobixz <166090657+nobixz@users.noreply.github.com>
2024-04-14 07:41:36 +0530

    Update play.py

--- DAXXMUSIC/utils/decorators/play.py
+++ DAXXMUSIC/utils/decorators/play.py
@@ -9,9 +9,9 @@ from pyrogram.errors import (
 )
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from DAXXMUSIC import YouTube, app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import (
+from NOBITAMUSIC import YouTube, app
+from NOBITAMUSIC.misc import SUDOERS
+from NOBITAMUSIC.utils.database import (
     get_assistant,
     get_cmode,
     get_lang,
@@ -20,7 +20,7 @@ from DAXXMUSIC.utils.database import (
     is_active_chat,
     is_maintenance,
 )
-from DAXXMUSIC.utils.inline import botplaylist_markup
+from NOBITAMUSIC.utils.inline import botplaylist_markup
 from config import PLAYLIST_IMG_URL, SUPPORT_CHAT, adminlist
 from strings import get_string
 
